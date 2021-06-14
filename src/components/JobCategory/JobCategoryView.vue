27d56648705d7e9f9b0d44f31ede154f4d53b2cb
Supreet <suprith.prakash.sp@gmail.com>
2021-06-14 11:39:58 +0530

    first commit

---
+++ src/components/JobCategory/JobCategoryView.vue
@@ -0,0 +1,24 @@
+<template>
+  <table border="1">
+      <thead>
+          <tr>
+              <td>
+                  id
+              </td>
+              <td>
+                  job category
+              </td>
+          </tr>
+      </thead>
+  </table>
+</template>
+
+<script>
+export default {
+name:'JobCategoryView'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,43 @@
+<template>
+  <form >
+      Email: <input type="email" v-model="email"><br>
+      Password: <input type="password" v-model="password"><br>
+      Role: <select v-model="role">
+          <option value="developer">.Net Developer</option>
+          <option value="tester">Tester</option>
+
+      </select>
+     <button @click="logvalues">signup</button>
+  </form>
+
+  <p>
+      {{email}}
+  </p>
+  <p>
+      {{password}}
+  </p>
+  <p>{{role}}</p>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+data(){
+    return{
+        email:'',
+        password:'',
+        role:''
+    }
+},
+methods:{
+    logvalues(){
+        console.warn(this.email)
+        
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
