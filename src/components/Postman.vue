c9178426e2416e4af7c7e2d8d029e65252757c56
Supreet <suprith.prakash.sp@gmail.com>
2021-06-26 00:10:12 +0530

    Get Method for Postman refactored

--- src/components/Postman.vue
+++ src/components/Postman.vue
@@ -1,55 +1,60 @@
 <template>
   <form action="">
-      <select name="selectMethod" id="" v-model="input">
-          <option value="Get">GET</option>
-      </select>
-      <input type="url" v-model="inputs.apiUrl">
-      <button type="submit" @click="Send">Send</button>
+    <select name="selectMethod" id="" v-model="input">
+      <option value="Get">GET</option>
+    </select>
+    <input type="url" v-model="inputs.apiUrl" />
+    <button type="submit" @click="Send">Send</button>
   </form>
   <div>
     <div class="my-3" id="responseJsonBox">
-                <div class="form-group row">
-                    <label for="responseJsonText" class="col-sm-2 col-form-label">Response</label>
-                    <div class="col-sm-10">
-                        <!-- <textarea class="form-control" id="responseJsonText" rows="3">Your response will appear here</textarea> -->
+      <div class="form-group row">
+        <label for="responseJsonText" class="col-sm-2 col-form-label"
+          >Response</label
+        >
 
-                        // <pre id='responsePre' class="language-javascript"> <code id='responsePrism' class="language-javascript"> {{Response}} </code> </pre>
-                    </div>
-                </div>
-            </div>
-
-    
+        <textarea
+          name="response"
+          id="response"
+          cols="30"
+          rows="10"
+          v-model="listDataString"
+        >
+        </textarea>
+        
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-data(){
-    return{
-        inputs:{
-            httpmethod:'',
-            apiUrl:''
-        },
-        Response:[]
-    }
+  data() {
+    return {
+      inputs: {
+        httpmethod: "",
+        apiUrl: "",
+      },
+      listDataString: String,
+      Response: [],
+    };
+  },
 
-},
-
-    methods:{
-      async Send(e){
-           e.preventDefault();
-          const response= await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-          console.log(response.data);
-          this.Response=response.data
-           
-       } 
-    }
-
-
-}
+  methods: {
+    async Send(e) {
+      e.preventDefault();
+      const response = await axios.get(
+       this.inputs.apiUrl
+      );
+      this.listDataString = JSON.stringify(response.data, null, "\t");
+      
+      this.Response = response.data;
+      console.log(this.Response);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
