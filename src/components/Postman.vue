9c91f2c05b824747ba0d8bab1f091c49b4ee49c0
Supreet <suprith.prakash.sp@gmail.com>
2021-06-26 14:33:10 +0530

    Post method updated

--- src/components/Postman.vue
+++ src/components/Postman.vue
@@ -2,9 +2,12 @@
   <form action="">
     <select name="selectMethod" id="" v-model="input">
       <option value="Get">GET</option>
+      <option value="Post">POST</option>
     </select>
     <input type="url" v-model="inputs.apiUrl" />
-    <button type="submit" @click="Send">Send</button>
+    <button type="submit" @click="Post" class="bt">Send</button><br><br>
+    <label for="request">Request Body</label>
+    <textarea v-model="inputs.body">Request Body</textarea>
   </form>
   <div>
     <div class="my-3" id="responseJsonBox">
@@ -35,6 +38,9 @@ export default {
       inputs: {
         httpmethod: "",
         apiUrl: "",
+            body:{
+         
+      }
       },
       listDataString: String,
       Response: [],
@@ -42,6 +48,7 @@ export default {
   },
 
   methods: {
+   
     async Send(e) {
       e.preventDefault();
       const response = await axios.get(
@@ -52,6 +59,18 @@ export default {
       this.Response = response.data;
       console.log(this.Response);
     },
+
+    async Post(e){
+        e.preventDefault();
+        var data = JSON.stringify(this.inputs.body);
+        console.log(data)
+        var  result =await axios
+        .post("https://reqres.in/api/users",this.inputs.body )
+        
+        console.log(result.data)
+        this.listDataString=JSON.stringify(result.data,null,"\t")
+
+    }
   },
 };
 </script>
